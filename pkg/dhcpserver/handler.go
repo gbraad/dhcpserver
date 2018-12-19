4f6c153352c0bcf685bfb3a14f08789a50ed6ae8
Gerard Braad <me@gbraad.nl>
2018-12-19 05:58:25 +0000

    Refactor

--- pkg/dhcpserver/handler.go
+++ pkg/dhcpserver/handler.go
@@ -9,83 +9,100 @@ import (
         dhcp "github.com/krolaw/dhcp4"
 )
 
-func (h *DHCPHandler) ServeDHCP(p dhcp.Packet, msgType dhcp.MessageType, options dhcp.Options) (d dhcp.Packet) {
-	switch msgType {
+func (handler *DHCPHandler) handleDiscover(packet dhcp.Packet, options dhcp.Options) (d dhcp.Packet) {
+	log.Println("Discover")
 
-	case dhcp.Discover:
-		log.Println("Discover")
+	offeredIP := net.IPv4zero
+	free, nic := -1, packet.CHAddr().String()
+	log.Println("  MAC:", nic)
 
-		offeredIP := net.IPv4zero
-		free, nic := -1, p.CHAddr().String()
-		log.Println("  MAC:", nic)
-
-		// Find previous lease
-		for i, v := range h.leases {
-			if v.nic == nic {
-				free = i
-				goto reply  // Yuck!
-			}
+	// Find previous lease
+	for i, v := range handler.leases {
+		if v.nic == nic {
+			free = i
+			goto reply  // Yuck!
 		}
+	}
 
-		// Static assignment
-		for _, v := range staticAssignments {
-			if v.nic == nic {
-				offeredIP = v.ip
-				goto reply  // Yuck!
-			}
+	// Static assignment
+	for _, v := range staticAssignments {
+		if v.nic == nic {
+			offeredIP = v.ip
+			goto reply  // Yuck!
 		}
+	}
 
-		// Find a free lease (based on range)
-		if free = h.freeLease(); free == -1 {
-			return
-		}
-	reply:
-		if offeredIP.Equal(net.IPv4zero) {  // no static
-			offeredIP = dhcp.IPAdd(h.start, free)
-		}
-		log.Println("  Reply", offeredIP)
+	// Find a free lease (based on range)
+	if free = handler.freeLease(); free == -1 {
+		return
+	}
+	
+reply:
+	if offeredIP.Equal(net.IPv4zero) {  // no static
+		offeredIP = dhcp.IPAdd(handler.start, free)
+	}
+	log.Println("  Reply", offeredIP)
 
-		return dhcp.ReplyPacket(p, dhcp.Offer, h.ip, offeredIP, h.leaseDuration,
-			h.options.SelectOrderOrAll(options[dhcp.OptionParameterRequestList]))
+	return dhcp.ReplyPacket(packet, dhcp.Offer, handler.ip, offeredIP, handler.leaseDuration,
+		handler.options.SelectOrderOrAll(options[dhcp.OptionParameterRequestList]))
+	
+}
 
-	case dhcp.Request:
-		log.Println("Request")
+func (handler *DHCPHandler) handleRequest(packet dhcp.Packet, options dhcp.Options) (d dhcp.Packet) {
+	log.Println("Request")
 
-		if server, ok := options[dhcp.OptionServerIdentifier]; ok && !net.IP(server).Equal(h.ip) {
-			return nil // Message not for this dhcp server
-		}
-		requestedIP := net.IP(options[dhcp.OptionRequestedIPAddress])
-		log.Println("  Requested:", requestedIP)
-		if requestedIP == nil {
-			requestedIP = net.IP(p.CIAddr())
-		}
+	if server, ok := options[dhcp.OptionServerIdentifier]; ok && !net.IP(server).Equal(handler.ip) {
+		return nil // Message not for this dhcp server
+	}
+	requestedIP := net.IP(options[dhcp.OptionRequestedIPAddress])
+	log.Println("  Requested:", requestedIP)
+	if requestedIP == nil {
+		requestedIP = net.IP(packet.CIAddr())
+	}
 
-		if len(requestedIP) == 4 && !requestedIP.Equal(net.IPv4zero) {
-			if leaseNum := dhcp.IPRange(h.start, requestedIP) - 1; leaseNum >= 0 && leaseNum < h.leaseRange {
-				nic := p.CHAddr().String()
-				log.Println("  MAC:", nic)
-				if l, exists := h.leases[leaseNum]; !exists || l.nic == nic {
-					h.leases[leaseNum] = DHCPLease{ nic: nic, expiry: time.Now().Add(h.leaseDuration) }
-					log.Println("  Reply - ACK", requestedIP)
-					return dhcp.ReplyPacket(p, dhcp.ACK, h.ip, requestedIP, h.leaseDuration,
-						h.options.SelectOrderOrAll(options[dhcp.OptionParameterRequestList]))
-				}
-			}
-		}
-		log.Println("  Reply - NAK")
-		return dhcp.ReplyPacket(p, dhcp.NAK, h.ip, nil, 0, nil)
-
-	case dhcp.Release, dhcp.Decline:
-		log.Println("Release/Decline")
-		nic := p.CHAddr().String()
-		log.Println("  MAC:", nic)
-		for i, v := range h.leases {
-			if v.nic == nic {
-				delete(h.leases, i)
-				break
+	if len(requestedIP) == 4 && !requestedIP.Equal(net.IPv4zero) {
+		if leaseNum := dhcp.IPRange(handler.start, requestedIP) - 1; leaseNum >= 0 && leaseNum < handler.leaseRange {
+			nic := packet.CHAddr().String()
+			log.Println("  MAC:", nic)
+			if l, exists := handler.leases[leaseNum]; !exists || l.nic == nic {
+				handler.leases[leaseNum] = DHCPLease{ nic: nic, expiry: time.Now().Add(handler.leaseDuration) }
+				log.Println("  Reply - ACK", requestedIP)
+				return dhcp.ReplyPacket(packet, dhcp.ACK, handler.ip, requestedIP, handler.leaseDuration,
+					handler.options.SelectOrderOrAll(options[dhcp.OptionParameterRequestList]))
 			}
 		}
 	}
+
+	log.Println("  Reply - NAK")
+	return dhcp.ReplyPacket(packet, dhcp.NAK, handler.ip, nil, 0, nil)
+
+}
+
+func (handler *DHCPHandler) handleReleaseDecline(packet dhcp.Packet, options dhcp.Options) (d dhcp.Packet) {
+	log.Println("Release/Decline")
+	nic := packet.CHAddr().String()
+	log.Println("  MAC:", nic)
+	for i, v := range handler.leases {
+		if v.nic == nic {
+			delete(handler.leases, i)
+			break
+		}
+	}
+
+	return nil
+}
+
+func (handler *DHCPHandler) ServeDHCP(packet dhcp.Packet, msgType dhcp.MessageType, options dhcp.Options) (d dhcp.Packet) {
+	switch msgType {
+
+	case dhcp.Discover:
+		return handler.handleDiscover(packet, options)
+	case dhcp.Request:
+		return handler.handleRequest(packet, options)
+	case dhcp.Release, dhcp.Decline:
+		return handler.handleReleaseDecline(packet, options)
+	}
+	
 	return nil
 }
 
